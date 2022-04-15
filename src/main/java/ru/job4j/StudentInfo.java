2c270b5ab83c69422b98bd7d01f71a4df3ea41d4
alisa <12345678>
2022-04-15 18:59:48 +0300

    После прохождения урока про checkstyle исправил ошибки, который бот находил в уроках 4.1 и 4.2

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Alexander Kireev");
         System.out.println("30.08.1989");
     }

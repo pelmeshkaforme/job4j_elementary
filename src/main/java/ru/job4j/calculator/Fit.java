540a38c9914f3a6c3cbb86ff50d895167ae8494f
alisa <12345678>
2022-04-17 00:25:51 +0300

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        double rsl = (height - 100) * 1.15;
+        return rsl;
+    }
+
+    public static double womanWeight(short height) {
+        double rsl = (height - 110) * 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        double man = Fit.manWeight(height);
+        double woman = Fit.womanWeight(height);
+        System.out.println("Ideal weight for man with " + height + " santimeters" + " height" + " - " + man + " kilograms");
+        System.out.println("Ideal weight for woman with " + height + " santimeters" + " height" + " - " + woman + " kilograms");
+    }
+}
+

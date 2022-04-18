67208122b274bcbd4fdfd36d11001b42e0c8922d
alisa <12345678>
2022-04-18 22:57:52 +0300

    исправление 0. Что такое тестирование [#159431 #290193]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -18,29 +18,26 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float inruToEu = 140;
-        float expectedruToEu = 2;
-        float outruToEu = Converter.ruToEu(inruToEu);
-        boolean passedruToEu = expectedruToEu == outruToEu;
+        float in = 140;
+        float expected = 2;
+        float out = Converter.ruToEu(in);
+        boolean passed = expected == out;
         float euro = Converter.ruToEu(140);
-        System.out.println("140 rubles are " + euro + " euros. Test result: " + passedruToEu);
-        float inruToDol = 140;
-        float expectedruToDol = 2.33333333F;
-        float outruToDol = Converter.ruToDol(inruToDol);
-        boolean passedruToDol = expectedruToDol == outruToDol;
+        System.out.println("140 rubles are " + euro + " euros. Test result: " + passed);
+        expected = 2.33333333F;
+        out = Converter.ruToDol(in);
+        passed = expected == out;
         float dollar = Converter.ruToDol(140);
-        System.out.println("140 rubles are " + dollar + " dollars. Test result: " + passedruToDol);
-        float ineuToRu = 140;
-        float expectedeuToRu = 9800;
-        float outeuToRu = Converter.euToRu(ineuToRu);
-        boolean passedeuToRu = expectedeuToRu == outeuToRu;
+        System.out.println("140 rubles are " + dollar + " dollars. Test result: " + passed);
+        expected = 9800;
+        out = Converter.euToRu(in);
+        passed = expected == out;
         float rubles1 = Converter.euToRu(140);
-        System.out.println("140 euros are " + rubles1 + " rubles. Test result: " + passedeuToRu);
-        float indolToRu = 140;
-        float expecteddolToRu = 8400;
-        float outdolToRu = Converter.dolToRu(indolToRu);
-        boolean passeddolToRu = expecteddolToRu == outdolToRu;
+        System.out.println("140 euros are " + rubles1 + " rubles. Test result: " + passed);
+        expected = 8400;
+        out = Converter.dolToRu(in);
+        passed = expected == out;
         float rubles2 = Converter.dolToRu(140);
-        System.out.println("140 doolars are " + rubles2 + " rubles. Test result: " + passeddolToRu);
+        System.out.println("140 doolars are " + rubles2 + " rubles. Test result: " + passed);
     }
 }

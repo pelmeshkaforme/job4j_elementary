d31d2899c7ae91b38801e5b7b3b59c989925bf4a
alisa <12345678>
2022-04-19 10:58:35 +0300

    еще промежуточный

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -13,14 +13,12 @@ public class MathCalculator {
     }
 
     public static double sumAndMultiplyAndDifferenceAndDivision(double first, double second) {
-        return sum(first, second) + multiply(first, second)
-                + difference(first, second) + division(first, second);
+        return sum(first, second) + multiply(first, second) + difference(first, second) + division(first, second);
     }
 
     public static void main(String[] args) {
         System.out.println("Результат сложения суммы и произведения равен = " + sumAndMultiply(10, 20));
         System.out.println("Результат сложения разности и деления равен = " + differenceandDivision(10, 20));
-        System.out.println("Результат сложения суммы, произведения, разности и деления равен = "
-                + sumAndMultiplyAndDifferenceAndDivision(10, 20));
+        System.out.println("Результат сложения суммы, произведения, разности и деления равен = " + sumAndMultiplyAndDifferenceAndDivision(10, 20));
     }
 }

75daee6d1f4949c8cf92d2066b487e98b94ca8e4
alisa <12345678>
2022-04-19 10:56:05 +0300

    исправление 1.1. Что такое import [#504773 #290343]

---
+++ src/test/java/ru/job4j/calculator/MathCalculatorTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MathCalculatorTest {
+
+    @Test
+    public void sumAndMultiply() {
+    }
+
+    @Test
+    public void differenceandDivision() {
+    }
+
+    @Test
+    public void sumAndMultiplyAndDifferenceAndDivision() {
+    }
+}

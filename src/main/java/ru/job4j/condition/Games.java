374c2d4b8251dc06208e3c54fe8295baa14d2fe3
alisa <12345678>
2022-04-21 14:43:51 +0300

    bсправление 4.3.1. Оператор && [#333783 #291637]

--- src/main/java/ru/job4j/condition/Games.java
+++ src/main/java/ru/job4j/condition/Games.java
@@ -2,13 +2,12 @@ package ru.job4j.condition;
 
 public class Games {
 
-    public static boolean permit(boolean allowParents, boolean hasMoney) {
+    public static void permit(boolean allowParents, boolean hasMoney) {
         if (allowParents && hasMoney) {
             System.out.println("I can go to club");
         } else {
             System.out.println("I can't go to club");
         }
-        return allowParents && hasMoney;
     }
 
     public static void main(String[] args) {

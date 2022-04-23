4ce2c9607a3d6bf0875137cba757899f548c4165
alisa <12345678>
2022-04-23 12:44:09 +0300

    исправление 4.6.1. Switch [#334070]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -3,30 +3,16 @@ package ru.job4j.condition;
 public class SwitchWeek {
 
     public static String nameOfDay(int number) {
-        String name = "Ошибка - для такого числа нет дня недели";
-        switch (number) {
-            case 1:
-                name = "Понедельник";
-                break;
-            case 2:
-                name = "Вторник";
-                break;
-            case 3:
-                name = "Среда";
-                break;
-            case 4:
-                name = "Четверг";
-                break;
-            case 5:
-                name = "Пятница";
-                break;
-            case 6:
-                name = "Суббота";
-                break;
-            case 7:
-                name = "Воскресенье";
-                break;
-        }
+        String name = switch (number) {
+            case 1 -> "Понедельник";
+            case 2 -> "Вторник";
+            case 3 -> "Среда";
+            case 4 -> "Четверг";
+            case 5 -> "Пятница";
+            case 6 -> "Суббота";
+            case 7 -> "Воскресенье";
+            default -> "Ошибка - для такого числа нет дня недели";
+        };
         return name;
     }
 

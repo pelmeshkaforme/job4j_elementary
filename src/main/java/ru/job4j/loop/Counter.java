648b9c1bd038698d15b4d99f7c07e120311f0ba6
alisa <12345678>
2022-04-24 22:35:56 +0300

    исправление 5.0.1. Сумма чисел. [#226416]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -2,17 +2,17 @@ package ru.job4j.loop;
 
 public class Counter {
 
-    public static int sum(int start, int finish) {
-        int sum = 0;
-        for (int i = start; i <= finish; i++) {
-            sum = sum + i;
-        }
-        return sum;
+        public static int sum(int start, int finish) {
+            int sum = 0;
+            for (int i = start; i <= finish; i++) {
+                sum = sum + i;
+            }
+            return sum;
         }
 
-    public static void main(String[] args) {
-        System.out.println(sum(3, 6));
-        System.out.println(sum(5, 9));
-        System.out.println(sum(8, 3));
+        public static void main(String[] args) {
+            System.out.println(sum(3, 6));
+            System.out.println(sum(5, 9));
+            System.out.println(sum(8, 3));
+        }
     }
-}

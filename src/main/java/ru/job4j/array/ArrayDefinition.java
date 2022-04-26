53befdb72fa5907a66f6886021c1b1d5e7cd97a6
alisa <12345678>
2022-04-26 10:16:03 +0300

    r

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,9 +8,8 @@ public class ArrayDefinition {
         names[1] = "Andrei";
         names[2] = "Anna";
         names[3] = "Olga";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
--- src/main/java/ru/job4j/live/Loop.java
+++ src/main/java/ru/job4j/live/Loop.java
@@ -2,9 +2,13 @@ package ru.job4j.live;
 
 public class Loop {
 
-    public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
+    public static void out(int n) {
+        for (int i = 0; i < n; n++) {
             System.out.println(i);
         }
+    }
+
+    public static void main(String[] args) {
+        out(5);
     }
 }

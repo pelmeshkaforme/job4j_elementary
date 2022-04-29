e73df873db56b179c129d7e13bc0b5080747007c
alisa <12345678>
2022-04-29 08:33:09 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -2,6 +2,17 @@ package ru.job4j.array;
 
 public class FindLoop {
 
+    public static int searchBetween(int[] data, int element, int from, int to) {
+        int rsl = -1;
+        for (int i = from; i <= to; i++) {
+            if (data[i] == element) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
+
     public static int indOf(int[] data, int element) {
         int rsl = -1;
         for (int i = 0; i < data.length; i++) {
@@ -14,8 +25,9 @@ public class FindLoop {
     }
 
     public static void main(String[] args) {
-        int[] data = new int[] {1, 4, 5, 6, 8};
+        int[] data = new int[] {1, 4, 5, 6, 8, 9, 6, 3};
         System.out.println(indOf(data, 5));
+        System.out.println(searchBetween(data, 4, 3, 7));
     }
 
 }
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -24,4 +24,22 @@ public class FindLoopTest {
         int expected = -1;
         Assert.assertEquals(expected, rsl);
     }
+
+    @Test
+    public void whenSearch6From2To6Then3() {
+        int[] data = {1, 4, 5, 6, 8, 7, 5, 4};
+        int element = 6;
+        int rsl = FindLoop.searchBetween(data, 6, 2, 6);
+        int expected = 3;
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenSearch4From3To7ThenMinus1() {
+        int[] data = {1, 4, 5, 6, 8, 7, 5, 3, 6};
+        int element = 4;
+        int rsl = FindLoop.searchBetween(data, 4, 3, 7);
+        int expected = -1;
+        Assert.assertEquals(expected, rsl);
+    }
 }
